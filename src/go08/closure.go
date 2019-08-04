1f5f0038985885c34ea4942f6c88edd4fac418d7
liushuzheng <liushuzheng@mininglamp.com>
2019-08-04 12:45:57 +0800

    closuse test for loop

--- src/go08/closure.go
+++ src/go08/closure.go
@@ -2,10 +2,7 @@ package main
 
 import "fmt"
 
-/**
-闭包代码测试
-*/
-
+/* 闭包代码测试 */
 func intSeq() func() int {
 
 	i := 0
@@ -16,8 +13,19 @@ func intSeq() func() int {
 }
 
 func main() {
-	nextInt := intSeq()
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
+	// nextInt := intSeq()
+	// fmt.Println(nextInt())
+	// fmt.Println(nextInt())
+	// fmt.Println(nextInt())
+	var fs []func() int
+	for i := 0; i < 3; i++ {
+		fs = append(fs, func() int {
+			fmt.Println("i is ", i)
+			return i
+		})
+	}
+	for _, f := range fs {
+		fmt.Printf("%p = %v\n", f, f())
+	}
+	//输出的都是3 闭包是在调用的时候才会调用函数中的变量
 }
