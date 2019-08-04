ff05f5123d9cac34043b88de0d62a912c46dcdeb
liushuzheng <liushuzheng@mininglamp.com>
2019-08-04 20:04:12 +0800

    some channel and data-type

--- src/go08/parallel/channel.go
+++ src/go08/parallel/channel.go
@@ -8,7 +8,8 @@ import (
 func fixedShooting(msgChannel chan string) {
 	for {
 		msgChannel <- "fixed shooting"
-		fmt.Println("continue fixed shooting ...")
+		time.Sleep(time.Second)
+		// fmt.Println("continue fixed shooting ...")
 	}
 }
 
@@ -16,14 +17,20 @@ func count(msgChannel chan string) {
 	for {
 		msg := <-msgChannel
 		fmt.Println(msg)
-		time.Sleep(time.Second)
 	}
 }
 
-func main() {
+func threePointShooting(msgChannel chan string) {
+	for {
+		msgChannel <- "three point shooting"
+	}
+}
+
+func main3() {
 	var c chan string
 	c = make(chan string)
 	go fixedShooting(c)
+	go threePointShooting(c)
 	go count(c)
 	var input string
 	fmt.Scan(&input)
---
+++ src/go08/parallel/channel2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fixedShooting2(msgChannel chan string) {
+	for {
+		msgChannel <- "fixed shooting"
+		time.Sleep(time.Second * 1)
+		// fmt.Println("continue fixed shooting ...")
+	}
+}
+
+func threePointShooting2(msgChannel chan string) {
+	for {
+		msgChannel <- "three point shooting"
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func main() {
+
+	cFixed := make(chan string)
+	c3Point := make(chan string)
+	go fixedShooting2(cFixed)
+	go threePointShooting2(c3Point)
+	go func() {
+		for {
+			select {
+			case msg1 := <-cFixed:
+				fmt.Println(msg1)
+			case msg2 := <-c3Point:
+				fmt.Println(msg2)
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scan(&input)
+}
