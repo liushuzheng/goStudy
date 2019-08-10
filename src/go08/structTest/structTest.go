66f8a2d8fab147fb56d2597afedc273b1db66295
liushuzheng <liushuzheng@mininglamp.com>
2019-08-10 23:30:24 +0800

    struct init method without warnings

--- src/go08/structTest/structTest.go
+++ src/go08/structTest/structTest.go
@@ -7,9 +7,10 @@ import (
 
 func main() {
 	//未使用key 警告
-	var rect = structandinterface.Rect{5, 6}
-	// rect.Width = 10.2
-	// rect.Length = 2
+	// var rect = structandinterface.Rect{5, 6}
+	var rect = structandinterface.Rect
+	rect.Width = 10.2
+	rect.Length = 2
 	fmt.Println((&rect).Area())
 	// 结构体遇到指针的时候 不需要使用传入地址
 	fmt.Println(rect.Area())
